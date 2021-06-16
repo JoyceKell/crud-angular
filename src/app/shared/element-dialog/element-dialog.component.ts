f1bd6d8ad7a9f6202601a2d79bef58a5683d4965
JoyceKell <joycekellywork@gmail.com>
2021-06-16 16:38:40 -0300

    crud utilizando material ui

---
+++ src/app/shared/element-dialog/element-dialog.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { PeriodicElement } from 'src/app/views/home/home.component';
+
+
+@Component({
+  selector: 'app-element-dialog',
+  templateUrl: './element-dialog.component.html',
+  styleUrls: ['./element-dialog.component.css']
+})
+export class ElementDialogComponent implements OnInit {
+  element!:PeriodicElement;
+  isChange!: boolean;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: PeriodicElement,
+    public dialogRef: MatDialogRef<ElementDialogComponent>,
+    ) {}
+
+  ngOnInit(): void {
+    if (this.data.position!=null){
+      this.isChange= true;
+    }
+    else{
+      this.isChange=false;
+    }
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
